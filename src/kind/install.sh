2cf30e6089b785b1b524e5ee2c8c8b1af5964c37
Michael Slats Slattery <Michael Slattery@hii-tsd.com>
2023-03-03 19:55:22 -0500

    wip

---
+++ src/kind/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------------------------------------------
+# Copyright (c) Michael Slattery. All rights reserved.
+# Licensed under the Apache 2 License. See LICENSE file for license information.
+#-------------------------------------------------------------------------------------------------------------
+#
+# Docs: TBD
+# Maintainer: Michael Slattery
+
+set -e
+
+echo "Activating feature 'kind'"
